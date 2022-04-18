a0fbf9f65c2579a26d5939d4a2617b664845343e
Konrad <konrad.danko@gmail.com>
2022-04-18 23:45:33 +0200

    Update of Dodatek

--- src/main/java/exercices/design_patterns/decorator/restaurant/skladnik_dekorator/Dodatek.java
+++ src/main/java/exercices/design_patterns/decorator/restaurant/skladnik_dekorator/Dodatek.java
@@ -1,7 +1,11 @@
-package exercices.design_patterns.decorator.restaurant;
+package exercices.design_patterns.decorator.restaurant.skladnik_dekorator;
+
+import exercices.design_patterns.decorator.restaurant.Danie;
 
 public class Dodatek extends Danie {
+  
   public Danie danie; //tu jest kwintesencja wzorca Dekorator
+
   public Dodatek(Danie danie) {
     this.danie = danie;
     this.opis = this.getClass().getSimpleName();
@@ -10,6 +14,7 @@ public class Dodatek extends Danie {
   public String getOpis() {
     return danie.getOpis() + "\n" + this.opis;
   }
+
   public double getKoszt() {
     return danie.getKoszt() + this.koszt;
   }
