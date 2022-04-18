a1cb7fa147962258feaec17d99c515a1660bfbd0
Konrad <konrad.danko@gmail.com>
2022-04-18 23:49:42 +0200

    Update of TestRestaurant

--- src/main/java/exercices/design_patterns/decorator/restaurant/TestRestaurant.java
+++ src/main/java/exercices/design_patterns/decorator/restaurant/TestRestaurant.java
@@ -1,5 +1,10 @@
 package exercices.design_patterns.decorator.restaurant;
 
+import exercices.design_patterns.decorator.restaurant.skladnik_dekorator.Kompot;
+import exercices.design_patterns.decorator.restaurant.skladnik_dekorator.Surowka;
+import exercices.design_patterns.decorator.restaurant.skladnik_dekorator.Ziemniaki;
+import exercices.design_patterns.decorator.restaurant.skladnik_glowny.Schabowy;
+
 public class TestRestaurant {
   public static void main(String[] args) {
     Danie danie = new Schabowy();
