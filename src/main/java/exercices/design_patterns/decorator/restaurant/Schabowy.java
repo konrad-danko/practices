62b53f601438c6eb2fca46540203a38f0ea3670e
Konrad <konrad.danko@gmail.com>
2022-04-18 23:48:48 +0200

    Update of Schabowy

--- src/main/java/exercices/design_patterns/decorator/restaurant/Schabowy.java
+++
@@ -1,7 +0,0 @@
-package exercices.design_patterns.decorator.restaurant;
-
-public class Schabowy extends DanieGlowne{
-  public Schabowy() {
-    this.koszt = 11.99;
-  }
-}
---
+++ src/main/java/exercices/design_patterns/decorator/restaurant/skladnik_glowny/Schabowy.java
@@ -0,0 +1,8 @@
+package exercices.design_patterns.decorator.restaurant.skladnik_glowny;
+
+public class Schabowy extends DanieGlowne {
+  
+  public Schabowy() {
+    this.koszt = 11.99;
+  }
+}
