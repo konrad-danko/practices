6dca243cf5a653314fb6f74874532bf43555381b
Konrad <konrad.danko@gmail.com>
2022-04-18 23:49:05 +0200

    Update of Danie

--- src/main/java/exercices/design_patterns/decorator/restaurant/Danie.java
+++ src/main/java/exercices/design_patterns/decorator/restaurant/Danie.java
@@ -1,9 +1,11 @@
 package exercices.design_patterns.decorator.restaurant;
 
 public abstract class Danie {
+  
   protected String opis;
   protected double koszt;
 
   public abstract String getOpis();
+
   public abstract double getKoszt();
 }
