cd672fcea373a84988119cd310006fdbd9585e75
konrad-danko <konrad.danko@gmail.com>
2022-11-02 21:48:03 +0100

    Update of 'decorator' package

--- src/main/java/exercices/design_patterns/decorator/star_cafe/Espresso.java
+++ src/main/java/exercices/design_patterns/decorator/star_cafe/Espresso.java
@@ -1,9 +1,11 @@
 package exercices.design_patterns.decorator.star_cafe;
 
 public class Espresso extends Napoj{
+
   public Espresso(){
     this.opis = "Kawa Espresso";
   }
+
   @Override
   public double koszt() {
     return 6.49;
