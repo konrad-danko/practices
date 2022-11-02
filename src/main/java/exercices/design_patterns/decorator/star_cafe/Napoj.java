9c47abdbbdf0339c8560c4344720526b8e6c2fb3
konrad-danko <konrad.danko@gmail.com>
2022-11-02 21:49:23 +0100

    Update of 'decorator' package

--- src/main/java/exercices/design_patterns/decorator/star_cafe/Napoj.java
+++ src/main/java/exercices/design_patterns/decorator/star_cafe/Napoj.java
@@ -1,9 +1,12 @@
 package exercices.design_patterns.decorator.star_cafe;
 
 public abstract class Napoj {
+
   public String opis;
+
   public String getOpis(){
   return this.opis;
   }
+
   public abstract double koszt();
 }
